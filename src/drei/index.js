4bc1369dd8ecfdce25b3bf23f33fb3c428ea6e56
Haichuan Zhang <me@std4453.com>
2019-03-28 16:39:12 +0800

    Remove material.js

--- src/drei/index.js
+++ src/drei/index.js
@@ -1,6 +1,5 @@
 import { Node, Group, Visible, Mesh, Camera, Material } from './classes';
 import { HUDCamera, OrthogonalCamera, PerspectiveCamera } from './camera';
-import { PureColorMaterial } from './material';
 import Tessellator from './tessellator';
 import Scene from './scene';
 import run from './run';
@@ -26,7 +25,6 @@ const wrapAll = (obj, params) => {
 const makeDrei = (gl, moreClasses) => wrapAll({
     Node, Group, Visible, Mesh, Camera, Material,
     HUDCamera, OrthogonalCamera, PerspectiveCamera,
-    PureColorMaterial,
     Tessellator,
     Scene,
     ...moreClasses,
